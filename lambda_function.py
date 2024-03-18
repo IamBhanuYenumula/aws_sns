d03625d133a8c8dbfe97e0352107f7199d20ef51
iambhanuprakash <130715988+iambhanuprakash@users.noreply.github.com>
2024-03-18 14:33:48 -0700

    test

--- lambda_function.py
+++ lambda_function.py
@@ -2,9 +2,8 @@ import json
 import random
 from datetime import datetime
 import boto3
-
+# imp main module
 def lambda_handler(event, context):
-    
     s3_client = boto3.client('s3')
     bucket = "gen-dd-json"
     key = ((datetime.now().date()).strftime("%Y-%m-%d"))+"-"+"raw_input.json"
@@ -15,10 +14,8 @@ def lambda_handler(event, context):
                 "amount":round(random.uniform(10,100),2),
                 "date":((datetime.now().date()).strftime("%Y-%m-%d"))}
         data_list.append(data)
-    ex = json.dumps(data_list)
-    
+    ex=json.dumps(data_list)   
     s3_client.put_object(Bucket=bucket,Key=key,Body=ex)
-    
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
